efa9dd670cc5f23831223565e143223f7b63a08f
bayicaidao <hhzh.meng@gmail.com>
2013-12-11 14:05:08 +0800

    Operation  并发

---
+++ OperationDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  OperationDemo
+//
+//  Created by ZhangMeng on 13-12-11.
+//  Copyright (c) 2013年 zhangM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ OperationDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  OperationDemo
+//
+//  Created by ZhangMeng on 13-12-11.
+//  Copyright (c) 2013年 zhangM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
